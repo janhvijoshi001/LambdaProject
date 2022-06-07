a3f5d1fb5e9f8ba1740beba6985c0a7b636d107c
janhvi joshi <51675618+janhvijoshi001@users.noreply.github.com>
2022-06-06 19:03:46 -0500

    updated some code add JSON jar and dependency (#1)

--- src/main/java/FirstOne/TestClass.java
+++ src/main/java/FirstOne/TestClass.java
@@ -1,7 +1,19 @@
 package FirstOne;
 
+import Second.EnumTesting;
+import Second.Rectangle;
+
 public class TestClass {
     public static void main(String[] args) {
         System.out.println("testing");
+        Rectangle obj = new Rectangle();
+        int result = obj.area(30,20);
+        System.out.println("result--> "+result);
+        System.out.println("result--> "+Rectangle.copy);
+
+        EnumTesting enumTesting = new EnumTesting(null);
+        System.out.println("----Order Details-----"+"\n"+enumTesting.details(EnumTesting.CoffeeSize.MEDIUM,38299765));
+        System.out.println("----Order Details-----"+"\n"+enumTesting.details(EnumTesting.CoffeeSize.SMALL,2222222,"janhvi joshi"));
+
     }
 }
